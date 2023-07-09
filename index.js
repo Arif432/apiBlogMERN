b2f7a9a90af928ccd0d8aebff5452c3c1f191999
MUHAMMAD ARIF NAWAZ <71346804+Arif432@users.noreply.github.com>
2023-07-09 22:52:41 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -119,10 +119,43 @@ app.get('/create', async(req,res)=>{
   const posts = await Post.find()
   .populate('author',['username'])
   .sort({createdAt:-1})
-  .limit(4)
+  .limit(20)
   // hitting the path on browser this result will show
   res.json(posts)
 })
+
+
+app.put('/post',uploadMiddleware.single('file'), async (req,res) => {
+  let newPath = null;
+  if (req.file) {
+    const {originalname,path} = req.file;
+    const parts = originalname.split('.');
+    const ext = parts[parts.length - 1];
+    newPath = path+'.'+ext;
+    fs.renameSync(path, newPath);
+  }
+
+  const {token} = req.cookies;
+  jwt.verify(token, secret, {}, async (err,info) => {
+    if (err) throw err;
+    const {id,title,summary,content} = req.body;
+    const postDoc = await Post.findById(id);
+    const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
+    if (!isAuthor) {
+      return res.status(400).json('you are not the author');
+    }
+    await postDoc.updateOne({
+      title,
+      summary,
+      content,
+      cover: newPath ? newPath : postDoc.cover,
+    });
+
+    res.json(postDoc);
+  });
+
+});
+
 
 app.get('/post/:id',async(req,res)=>{
 
@@ -130,5 +163,8 @@ app.get('/post/:id',async(req,res)=>{
   const postDoc = await Post.findById(id).populate('author',['username'])
   res.json(postDoc) 
 })
+
+
+
 
   app.listen(5000)
